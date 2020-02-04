14bde99f77672f8f31df68eae9d34386f0a1f5d4
mohsin rahman <mohsinrahmang@gmail.com>
2020-02-04 18:58:29 +0100

    Fixed firt two parts

--- client/index.php
+++ client/index.php
@@ -7,26 +7,115 @@
     <link href="//db.onlinewebfonts.com/c/5345fb51cb4f6e796b930b80cdf9af5f?family=Order+Pizza" rel="stylesheet" type="text/css"/>
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="css/style.css">
   
     <title>Pizza Box</title>
 </head>
 <body>
     <div class="Pizzabox-heroSection">
-        <div class="Pizzabox-heroSection-menu">home</div>
+
     </div>
-    <div class="Pizzabox-ourStory"></div>
+    <div class="Pizzabox-heroSection-bg">
+        <div class="row" style="border-bottom:1px solid #2c2c2c;">
+        <div class="col-lg-6 text-center"><h6 class="Pizzabox-heroSection-email" >Email: info@pizzabox.com Phone: +46 800 000 16</h6>  </div>
+        <div class="col-lg-6 text-center">
+            <div class="Pizzabox-heroSection-opening">
+                <div class="row">
+                    <div class="col-lg-8">
+                    Opening Hours: 8:00am - 11:30PM
+                    </div>
+                    
+                    <div class="col-lg-4"> 
+                    <a href="">Login</a>  <span>0</span><img src="images/bag.svg" height="15"  alt="cart">
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>  <!-- Pizzabox-heroSection-bg --> 
+        <div class="Pizzabox-heroSection-menu">
+            <div class="row" >
+                <div class="col-lg-4 text-center">
+                <ul class="list-unstyled list-inline">
+                    <li class="list-inline-item">HOME</li>
+                    <li class="list-inline-item">MENU</li>
+                    <li class="list-inline-item">OFFER</li>
+                </ul>
+                </div>
+                <div class="col-lg-4 text-center">
+                <img src="images/logo7.svg" width="200" alt="LOGO">
+                </div>
+                <div class="col-lg-4 text-center">
+                <ul class="list-unstyled list-inline">
+                    <li class="list-inline-item">CONTACT</li>
+                    <li class="list-inline-item">BLOG</li>
+                    <li class="list-inline-item">ABOUT US</li>
+                </ul>
+                </div>
+             </div>
+        </div>
+        <div class="row Pizzabox-heroSection-content">
+            <div class="col-lg-12 text-center mt-5">
+                <h3 class="pt-5">TASTY & SPICY</h3>
+                <h5 class="pt-5">World's Delicious Cuisine</h5>
+                <button class="mt-5">ORDER NOW</button>
+            </div>
+        </div>
+       
+    </div>
+
+
+    <div class="Pizzabox-ourStory">
+        <h3>Our Story</h3>
+        <div class=" Pizzabox-ourStory-content row">
+         <div class="col-lg-6">
+             <div class="row">
+                <div class="col-lg-6">
+                <img class="Pizzabox-ourStory-img1" src="images/img6.jpg" width="100%"  alt="img">
+                <img class="Pizzabox-ourStory-img2" src="images/img1.jpg" width="100%" alt="img">
+                </div>
+                <div class="col-lg-6">
+                <img class="Pizzabox-ourStory-img3" src="images/img3.jpg" width="100%" alt="img">
+                </div>
+
+             </div>
+         </div>
+         <div class="col-lg-6">
+           <div class="Pizzabox-ourStory-content-welcome">
+                <h5>Welcome to PizzaBox</h5>
+                <p>Quisque sit amet turpis etipsum elementum facilis. Quisque sed placerat libero. Pellentesque nec tellu
+                    sollicitudin, sollicitudin ligula non, tristique nibh Donec vitae turpis sagittis, cursus nunc ac, aliquet.
+                    Donec viverra vel massa aaugue consequat posuere sem, eu ornares viverra veleso massa at  posuere. Aliquam 
+                    et fringilla au. Quisque sit amet turpis etipsum elementum facilis.Quisque sed placerat libero. Pellentesque
+                    nec tellu sollicitudin, sollicitudin ligula non, tristique nibh  Donec vitae turpis sagittis, cursus nunc ac, aliquet.
+                    Donec viverra vel massa aaugue consequat posuere sem, eu ornares viverra veleso massa at posuere. Aliquam et fringilla au.
+                </p>
+                <img  class="Pizzabox-ourStory-content-sign" src="images/sign.png" alt="Sign">
+                <div class="row">
+                    <div class="col-lg-4">
+                    <img class="Pizzabox-ourStory-content-chef" src="images/chef1.jpg" alt="Chef">
+                    </div>
+                    <div class="col-lg-8 Pizzabox-ourStory-content-name">
+                     <h4>John Conery</h4>
+                     <h6>Master Chef</h6>
+                    </div>
+                </div>
+            </div>
+         </div>
+        </div>
+
+    </div>
+
     <div class="Pizzabox-WeekSpecial"></div>
     <div class="Pizzabox-stunningThings"></div>
     <div class="Pizzabox-video"></div>
     <div class="Pizzabox-menu"></div>
     <div class="Pizzabox-contactUs"></div>
     <div class="Pizzabox-footer"></div>
-    <div> test</div>
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="../js/script.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
