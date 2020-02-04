b7ce1c6b6e0d726bf3e8988b7f0cd40f04ef7e6f
Amir <56071054+AmirShishehgari@users.noreply.github.com>
2020-02-04 09:55:26 +0100

    footer is ready now
    fixed

--- client/index.php
+++ client/index.php
@@ -78,8 +78,28 @@
     <div class="Pizzabox-video"></div>
     <div class="Pizzabox-menu"></div>
     <div class="Pizzabox-contactUs"></div>
-    <div class="Pizzabox-footer"></div>
-    <div> test</div>
+    <div class="footer py-2 bg-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="footer-content d-flex align-items-center justify-content-between">
+                            <div class="left">
+                                <span class="d-block text-white">Copyright @ 2020 PizzaBox</span>
+                                <span class="text-white">All rights reserved</span>
+                            </div>
+                            <div class="center">
+                                <img src="images/pizzagreen.jpg" width="80" height="80" alt="">
+                            </div>
+                            <div class="right">
+                                <span class="text-white mx-2 text-danger">Terms & Conditions</span>
+                                <span class="text-white">Privcy Policy</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
