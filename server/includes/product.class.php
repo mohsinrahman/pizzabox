201c1295741419cff7ae792441ad1517e1d2928b
Polina Yaromchyk <pollyyaromchyk@gmail.com>
2020-02-07 13:22:05 +0100

    Class product

--- server/includes/product.class.php
+++ server/includes/product.class.php
@@ -20,16 +20,37 @@ class Product {
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
         if (empty($result)){
-            throw new Exception("No students found", 404);
+            throw new Exception("No products found", 404);
             exit;
         }
 
         return $result;
     }
-    public function productInserts ($productID, $productName, $description, $unitPrice, $category, $unitsInStock,$image){
+    public function insertProduct ($productID, $productName, $description, $unitPrice, $category, $unitsInStock,$image){
         $sql = "INSERT INTO Products (ProductID, ProductName, Description, UnitPrice, Category, UnitsInStock, Image) VALUES (?,?,?,?,?,?,?)";
         $query = $this->db->link->prepare($sql)->execute([$productID, $productName, $description, $unitPrice, $category, $unitsInStock, $image]);;
      
+    }
+
+    public function getAllOfThisCategory($catergoryChoosen) {
+            
+        try{
+
+            $query = $this->db->link->prepare
+            (" SELECT ProductID FROM Products 
+            WHERE Category='$catergoryChoosen';");
+            $query->execute();
+            $query->setFetchMode(PDO::FETCH_OBJ);
+            $result = $query->fetchAll();
+            if (empty($result)) {
+                return false;
+            }
+            return $result;
+    
+        }catch(Exception $e) {
+            echo $e->getMessage();
+    
+        }
     }
 }
 
@@ -49,9 +70,9 @@ $result = $w->getAllProducts();
 
 echo json_encode($result);
 
-$w->productInserts(2,2,2,2,2,2,2);
+//$w->productInsert(2,2,2,2,2,2,2);
 
-
-
+$r=$w->getAllOfThisCategory("Pizza");
+echo json_encode($r);
 ?>
 
