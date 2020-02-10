b349ced72d4c904a226b9db46e21bd96290e27cd
mohsin rahman <mohsinrahmang@gmail.com>
2020-02-10 11:47:18 +0100

    update sign up and sign in

--- client/index.php
+++ client/index.php
@@ -10,11 +10,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.12.0/css/mdb.min.css" rel="stylesheet">
     <title>Pizza Box</title>
     
 </head>
 <body>
-<!-- Modal -->
+<!-- Modal Cart -->
 <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
@@ -127,7 +128,18 @@
                 </div>
         </div>
       </div>
-       <p class="modal-body-heading product pt-3">Payment Method</p>
+      <p class="modal-body-heading product pt-4">Delivery Time</p>
+      <div class="container">
+        <div class="row">
+                <div class="col-lg-12 ">                
+                    <div class="form-group">
+                    <label for="exampleFormControlTextarea2">Mention time and date.</label>
+                    <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3"></textarea>
+                    </div>                    
+                </div>
+        </div>
+      </div>
+       <p class="modal-body-heading product pt-4">Payment Method</p>
       <div class="container">
 
         <div class="row">
@@ -202,6 +214,15 @@
       <input type="password" class="form-control" id="inputPassword" placeholder="Password">
     </div>
   </div>
+
+  <div class="form-group row"> 
+    <label class="form-check-label col-sm-2 col-form-label" for="adminChecked">Admin</label>
+    <div class="form-check"> 
+        <div class="col-sm-10">  
+            <input type="checkbox" class="form-check-input" id="adminChecked" checked>
+        </div>        
+    </div>
+  </div>
  
   <div class="form-group row">
     <div class="col-sm-5">
@@ -238,20 +259,28 @@
       <div class="form-group row">
                 <label for="inputName3" class="col-sm-2 col-form-label">Name</label>
                 <div class="col-sm-10">
-                <input type="text" class="form-control" id="name" placeholder="Name">
+                    <input type="text" class="form-control" id="name" placeholder="Name">
                 </div>
         </div>
         
         <div class="form-group row">
                 <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
-                <input type="email" class="form-control" id="email" placeholder="Email">
+                    <input type="email" class="form-control" id="email" placeholder="Email">
                 </div>
         </div>
          <div class="form-group row">
                 <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
                 <div class="col-sm-10">
-                <input type="password" class="form-control" id="password" placeholder="Password">
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+        </div>
+        <div class="form-group row"> 
+                <label class="form-check-label col-sm-2 col-form-label" for="adminChecked">Admin</label>
+                <div class="form-check"> 
+                <div class="col-sm-10">  
+                    <input type="checkbox" class="form-check-input" id="adminChecked" checked>
+                </div>        
                 </div>
         </div>
 
