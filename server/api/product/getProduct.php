c3ecec17e8ecde213225da161142f05a424585af
Polina Yaromchyk <pollyyaromchyk@gmail.com>
2020-02-10 11:30:02 +0100

    API

---
+++ server/api/product/getProduct.php
@@ -0,0 +1,31 @@
+<?php
+include("../../includes/product.class.php");
+$method = isset($_SERVER['REQUEST_METHOD']);
+if($method){
+    if($method === 'GET') {
+        
+        
+        $product = new Product();
+        
+        if(isset($_GET['Category'])) {
+                 
+                $catergoryChoosen = $_GET['Category'];
+                if($catergoryChoosen == "Pizza" || $catergoryChoosen == "Side dish" || $catergoryChoosen == "Dessert" || $catergoryChoosen == "Drink"){
+
+                    $products = $product->getAllOfThisCategory($catergoryChoosen);
+                    echo json_encode($products);
+                }else {
+                    $allProducts = $product->getAllProducts();
+                    echo json_encode($allProducts);
+                }
+
+        
+        
+
+        } else {
+            echo json_encode("No category.");
+        }
+    }
+}
+
+?>
