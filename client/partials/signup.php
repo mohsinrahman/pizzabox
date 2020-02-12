37d2811a8ebc352e0a06ea47a6fe7eebfd2734cf
Amir <56071054+AmirShishehgari@users.noreply.github.com>
2020-02-12 18:11:09 +0100

    Update signup.php
    added name

--- client/partials/signup.php
+++ client/partials/signup.php
@@ -13,41 +13,41 @@ aria-hidden="true">
     <div class="form-group row">
               <label for="firstName" class="col-sm-3 col-form-label">First Name</label>
               <div class="col-sm-9">
-                  <input type="text" class="form-control" id="fname" placeholder="First Name">
+                  <input type="text" name="fname" class="form-control" id="fname" placeholder="First Name">
               </div>
       </div>
 
       <div class="form-group row">
               <label for="lastName" class="col-sm-3 col-form-label">Last Name</label>
               <div class="col-sm-9">
-                  <input type="text" class="form-control" id="lname" placeholder="Last Name">
+                  <input type="text" name="lname" class="form-control" id="lname" placeholder="Last Name">
               </div>
       </div>
 
       <div class="form-group row">
               <label for="phone" class="col-sm-3 col-form-label">Phone</label>
               <div class="col-sm-9">
-                  <input type="tel" class="form-control" id="phone" placeholder="Phone">
+                  <input type="tel" name="phone" class="form-control" id="phone" placeholder="Phone">
               </div>
       </div>
       
       <div class="form-group row">
               <label for="inputEmail3" class="col-sm-3 col-form-label">Email</label>
               <div class="col-sm-9">
-                  <input type="email" class="form-control" id="email" placeholder="Email">
+                  <input type="email" name="email" class="form-control" id="email" placeholder="Email">
               </div>
       </div>
        <div class="form-group row">
               <label for="inputPassword3" class="col-sm-3 col-form-label">Password</label>
               <div class="col-sm-9">
-                  <input type="password" class="form-control" id="password" placeholder="Password">
+                  <input type="password" name="password" class="form-control" id="password" placeholder="Password">
               </div>
       </div>
 
       <div class=" form-group row"> 
               <label class=" col-sm-3 col-form-label" for="adminChecked">Admin</label>
               <div class="col-sm-9"> 
-                  <input type="checkbox" class="form-control " id="CheckboxAdmin">        
+                  <input type="checkbox" name="admin" class="form-control " id="CheckboxAdmin">        
               </div>        
       </div>
 
